164eda7629d65a3acab7ad97091f3ead0d3d06a9
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2020-02-16 09:44:18 +0900

    Update Day6BitwiseOperators.js

--- Day6BitwiseOperators.js
+++ Day6BitwiseOperators.js
@@ -1,4 +1,4 @@
-// https://github.com/tatsuyafujisaki/hackerrank-10-days-of-javascript-solutions/blob/master/README.md
+// Explanation: https://github.com/tatsuyafujisaki/hackerrank-10-days-of-javascript-solutions/blob/master/README.md
 function getMaxLessThanK(n, k) {
   return n < ((k - 1) | k) ? (k - 2) : (k - 1);
 }
